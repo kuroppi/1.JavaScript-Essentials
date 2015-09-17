e257d46e6f0d4aa7bad635b439e69e58b597c940
B P <str.kuroppi@gmail.com>
2015-09-17 15:00:28 -0700

    Add Assignment1.html and script.js

---
+++ script.js
@@ -0,0 +1,333 @@
+//STEP 1
+
+/*
+var someMonth;
+function theMonth     // function to return current month
+currentMonth          // a constant
+var summerMonth;      // an array of summer months
+myLibraryAfunction    // a function
+*/
+
+
+//STEP 2
+
+/*
+5.5                     // numeric literal expression
+"Welcome!"              // string literal expression
+true                    // boolean literal expression
+null                    // nulll literal expression
+*/
+
+//STEP 3
+
+/*
+var salesTax = subtotal * .1;
+var totalNumber = 10 + (2 * 3) / 4;
+*/
+
+//STEP 4
+
+/*
+var sFirstName;
+var sLastName;
+var sAddress;
+var sCity;
+var sState;
+var sZipCode;
+var nYourAge;
+var sReferralSource;
+var bMayWeContactYou;
+*/
+
+//STEP 5
+
+/*
+var sFirstName;
+var sLastName;
+var sAddress;
+var sCity;
+var sState;
+var sZipCode;
+var nYourAge;
+var sReferralSource;
+var bMayWeContactYou;
+sFirstName = "John";
+sLastName = "Doe";
+sAddress = "8355 Aero Drive";
+sCity = "San Diego";
+sState = "California";
+sZipCode = "92123";
+nYourAge = 21;
+sReferralSource = "Friend";
+bMayWeContactYou = true;
+
+var sFirstName = "John";
+var sLastName = "Doe";
+var sAddress = "8355 Aero Drive";
+var sCity = "San Diego";
+var sState = "California";
+var sZipCode = "92123";
+var nYourAge = 21;
+var sReferralSource = "Friend";
+var bMayWeContactYou = true;
+
+var sFirstName = "John", sLastName = "Doe", sAddress = "8355 Aero Drive", sCity = "San Diego", sState = "California", sZipCode = "92123", nYourAge = 21, sReferralSource = "Friend", bMayWeContactYou = true;
+*/
+
+//STEP 6
+
+/*
+var firstVariable;
+var secondVariable;
+var thirdVariable;
+
+firstVariable = 1 + "Line of text";
+console.log(firstVariable);
+
+secondVariable = true + "Line of text";
+console.log(secondVariable);
+
+thirdVariable = 17 + false;
+console.log(thirdVariable);
+*/
+
+//STEP 7
+
+/*
+console.log("My favorite food is " + foodType + ".");
+var foodType = "pizza";
+*/
+
+//STEP 8
+
+/*
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+console.log (someString);
+*/
+
+//STEP 9
+
+/*
+var x = null;
+var y; 
+console.log(x);
+console.log(y);
+*/
+
+//STEP 10
+
+//var firstName = typeof "Bob";
+//var yourAge = typeof 21;
+//var currentStudent = typeof true;
+//var thisObject = typeof[firstName, yourAge, currentStudent];
+//var yourOccupation = typeof yourJob;
+//
+//console.log(firstName);
+//console.log(yourAge);
+//console.log(currentStudent);
+//console.log(thisObject);
+//console.log(yourOccupation);
+
+//STEP 11
+
+/*
+alert("Hello " + "Bob Painter" + ", welcome to the JavaScript class!");
+*/
+
+//STEP 12
+
+/*
+var name = "Bob Painter";
+alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
+
+//STEP 13
+
+/*
+var name = "Bob Painter";
+var course = "JavaScript";
+alert("Hello " + name + ", welcome to the " + course + " class!");
+*/
+
+//STEP 14
+
+/*
+var name = "Bob Painter";
+var course = "JavaScript";
+alert("Hello " + name + ", \nWelcome to the " + course + " class!");
+*/
+
+//STEP 15
+
+/*
+var name = prompt("What is your name?");
+var course = "JavaScript";
+alert("Hello " + name + ", \nWelcome to the " + course + " class!");
+*/
+
+//STEP 16
+
+/*
+var name = prompt("What is your name?");
+var course = prompt("What class are you taking?");
+alert("Hello " + name + ", \nWelcome to the " + course + " class!");
+*/
+
+//STEP 17
+
+/*
+var x = 10;
+var y = 20;
+console.log(x + y);
+*/
+
+//STEP 18
+
+/*
+var x = 20;
+console.log(x += 20);
+*/
+
+//STEP 19
+
+/*
+var x = 20;
+console.log(x *= 5);
+*/
+
+//STEP 20
+
+/*
+x = 20 % 3;
+x = x /= 1;
+console.log (x);
+*/
+
+//STEP 21
+
+/*
+var x = 5;
+var y = 20;
+console.log(x == 5 && y == 20);
+*/
+
+//STEP 22
+
+/*
+var x = 100;
+var y = 200;
+console.log(x == 200 || y == 100);
+*/
+
+//STEP 23
+
+/*
+var widget = new Object();
+console.log(typeof widget);
+*/
+
+//STEP 24
+
+/*
+var widget = new Object();
+console.log(widget instanceof Object);
+*/
+
+//STEP 25
+
+/*
+var widget = new Object();
+console.log(widget instanceof Object == widget);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
